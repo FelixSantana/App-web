4da8b2d5af00634e29d1c462fbfcfbcdb47fb9ae
Felix Santana <flexsantana99@gmail.com>
2025-11-06 22:41:48 -0400

    creacion del manu dinamico

--- script.js
+++ script.js
@@ -1,38 +1,136 @@
-const btnVerificar = document.getElementById('btnVerificar');
-const cedulaInput = document.getElementById('cedulaInput');
-const verificarCedula = (e)=>{
-    e.preventDefault();
-    let arregloCedula = cedulaInput.value.replace(/-/g,'').split("");
-    let arregloDigitos = [];
-    let arregloFinal = [];
-    let resultadoComparativo=0;
-    for(let i=1; i<=arregloCedula.length; i++){
-        if(i%2==0){
-            arregloDigitos.push(parseInt(arregloCedula[i-1])*2);
-        }
-        else{
-            arregloDigitos.push(parseInt(arregloCedula[i-1])*1);
-        }
-    }
-    for(let i=0; i<arregloCedula.length-1; i++){
-        if(arregloDigitos[i]>9){
-            let string = arregloDigitos[i].toString();
-            let suma = parseInt(string[0]) + parseInt(string[1]);
-            arregloFinal.push(suma);
-        }
-        else{
-            arregloFinal.push(arregloDigitos[i]);
-        }
-    }
-    arregloFinal.forEach(valor=>{
-        resultadoComparativo+=valor;
-    })
-    let modulo = resultadoComparativo%10;
-    if(10-modulo==arregloCedula[10]){
-        alert("La cedula ingresada es correcta");
-    }
-    else{
-        alert("La cedula ingresada es incorrecta");
-    }
+const contenedor = document.querySelector(".contenedor");
+const header_ul = document.querySelector(".header_ul");
+const btnAgregarOpcion = document.getElementById("btnAgregarOpcion");
+const btnActualizarOpcion = document.getElementById("btnActualizarOpcion");
+const btnEliminarOpcion = document.getElementById("btnEliminarOpcion");
+
+let datos = {
+    "menu": [
+        {
+            "id": 1,
+            "nombre": "Inicio",
+            "enlace": "/inicio"
+        },
+        {
+            "id": 2,
+            "nombre": "Sobre Nosotros",
+            "enlace": "/sobre-nosotros"
+        },
+        {
+            "id": 3,
+            "nombre": "Servicios",
+            "enlace": "/servicios"
+        },
+        {
+            "id": 4,
+            "nombre": "Contacto",
+            "enlace": "/contacto"
+        },
+    ]
 }
-btnVerificar.addEventListener("click",verificarCedula);
+// Codigo para agregar una funcion
+const agregarOpcion = (datos) => {
+    header_ul.innerHTML += `<li class="nav-item" id="${datos.nombre}"><a href="${datos.enlace}" class="nav-link" aria-current="page">${datos.nombre}</a></li>`;
+}
+//Agregando al menu las opciones por defecto
+datos.menu.forEach(item => {
+    agregarOpcion(item);
+});
+
+btnAgregarOpcion.addEventListener("click", () => {
+    document.getElementById("btnAgregarNuevaOpcion").addEventListener("click", () => {
+        const nuevaOpcionId = document.getElementById("nuevaOpcionId").value;
+        const nuevaOpcionNombre = document.getElementById("nuevaOpcionNombre").value;
+        const nuevaOpcionEnlace = document.getElementById("nuevaOpcionEnlace").value;
+        if (nuevaOpcionId == " " || nuevaOpcionNombre == " " || nuevaOpcionEnlace == "") {
+            alert("Debe llenar todos los campos");
+        }
+        else {
+            let nuevaOpcionJson = {
+                "id": parseInt(nuevaOpcionId),
+                "nombre": nuevaOpcionNombre,
+                "enlace": nuevaOpcionEnlace
+            }
+            datos.menu.push(nuevaOpcionJson);
+            agregarOpcion(nuevaOpcionJson);
+            alert("Opcion agregada correctamente");
+        }
+    })
+}, { once: true })
+
+//Codigo para actualizar una opcion
+btnActualizarOpcion.addEventListener("click", () => {
+    const modalActualizarOpcion = document.querySelector(".modalActualizarOpcion");
+    datos.menu.forEach(opcion => {
+        modalActualizarOpcion.innerHTML += `<input type="checkbox" id="${opcion.nombre}"><label for="${opcion.nombre}"> ${opcion.nombre}</label><br>`
+    })
+    const btnSiguinte = document.getElementById("btnSiguiente");
+    btnSiguinte.addEventListener("click", () => {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        const haySeleccionado = Array.from(checkboxes).some(checkbox => checkbox.checked);
+        if (haySeleccionado > 1) {
+            alert("Solo puede editar una opcion a la vez");
+        }
+        else if (haySeleccionado == 1) {
+            btnSiguinte.setAttribute("data-bs-target", "#exampleModalToggle2");
+            btnSiguinte.setAttribute("data-bs-toggle", "modal");
+            const opcionSelecionada = Array.from(checkboxes).find(checkbox => checkbox.checked);
+            datos.menu.forEach(opcion => {
+                if (opcion.nombre == opcionSelecionada.getAttribute("id")) {
+                    document.getElementById("actualizarOpcionId").value = opcion.id;
+                    document.getElementById("actualizarOpcionNombre").value = opcion.nombre;
+                    document.getElementById("actualizarOpcionEnlace").value = opcion.enlace;
+                    document.getElementById("btnGuardarCambios").addEventListener("click", () => {
+                        let id= document.getElementById("actualizarOpcionId").value;
+                        let nombre=document.getElementById("actualizarOpcionNombre").value;
+                        let enlace=document.getElementById("actualizarOpcionEnlace").value;
+                        if(id=="" || nombre=="" ||enlace==""){
+                            alert("Debe llenar todos los campos");
+                        }
+                        else{
+                           datos.menu.forEach(item=>{
+                            if(item.nombre==opcionSelecionada.getAttribute("id")){
+                                item.id = id;
+                                item.nombre = nombre;
+                                item.enlace = enlace;
+                                document.querySelectorAll(".nav-item").forEach(elemento=>{
+                                    header_ul.removeChild(elemento);
+                                })
+                                datos.menu.forEach(item => {
+                                    agregarOpcion(item);
+                                })
+                                alert("Opcion actualizada correctamente");
+                            }
+                           })
+                        }
+                    });
+                }
+            })
+        }
+        else {
+            alert("Ninguna opcion marcada");
+        }
+    },{once:true})
+}, { once: true })
+
+//Codigo para eliminar una opcion
+btnEliminarOpcion.addEventListener("click",()=>{
+    const eliminarOpcionModal = document.querySelector(".eliminarOpcionModal");
+    datos.menu.forEach(opcion => {
+        eliminarOpcionModal.innerHTML+= `<input type="checkbox" id="${opcion.nombre}"><label for="${opcion.nombre}"> ${opcion.nombre}</label><br>`
+    });
+    document.getElementById("btnEliminar").addEventListener("click",()=>{
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox=>{
+            if(checkbox.checked){
+                document.querySelectorAll(".nav-item").forEach(elemento=>{
+                    if(elemento.textContent==checkbox.getAttribute("id")){
+                        header_ul.removeChild(elemento);
+                        datos.menu = datos.menu.filter(opcion => opcion.nombre !== elemento.textContent);
+                        alert("Opcion eliminada correctamente");
+                    }
+                })
+            }         
+        })
+    })
+},{once:true})
