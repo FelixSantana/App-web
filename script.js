8153a738e7d9978daf72211376c401b8d2d2dd7c
Felix Santana <flexsantana99@gmail.com>
2025-12-19 00:11:51 -0400

    verificador de cedulas

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-
-document.getElementById("formContacto").addEventListener("submit", function (e) {
+const btnVerificar = document.getElementById('btnVerificar');
+const cedulaInput = document.getElementById('cedulaInput');
+const verificarCedula = (e)=>{
     e.preventDefault();
-    const nombre = document.getElementById("nombre").value.trim();
-    const telefono = document.getElementById("telefono").value.trim();
-    const email = document.getElementById("email").value.trim();
-
-    const vCard = `BEGIN:VCARD
-VERSION:3.0
-N:${nombre}
-FN:${nombre}
-TEL;TYPE=cell:${telefono}
-EMAIL:${email}
-END:VCARD`;
-
-    document.getElementById("qrcode").innerHTML = "";
-    new QRCode(document.getElementById("qrcode"), {
-        text: vCard,
-        width: 256,
-        height: 256,
-        correctLevel: QRCode.CorrectLevel.M
-    });
-});
+    let arregloCedula = cedulaInput.value.replace(/-/g,'').split("");
+    let arregloDigitos = [];
+    let arregloFinal = [];
+    let resultadoComparativo=0;
+    for(let i=1; i<=arregloCedula.length; i++){
+        if(i%2==0){
+            arregloDigitos.push(parseInt(arregloCedula[i-1])*2);
+        }
+        else{
+            arregloDigitos.push(parseInt(arregloCedula[i-1])*1);
+        }
+    }
+    for(let i=0; i<arregloCedula.length-1; i++){
+        if(arregloDigitos[i]>9){
+            let string = arregloDigitos[i].toString();
+            let suma = parseInt(string[0]) + parseInt(string[1]);
+            arregloFinal.push(suma);
+        }
+        else{
+            arregloFinal.push(arregloDigitos[i]);
+        }
+    }
+    arregloFinal.forEach(valor=>{
+        resultadoComparativo+=valor;
+    })
+    let modulo = resultadoComparativo%10;
+    if(10-modulo==arregloCedula[10]){
+        alert("La cedula ingresada es correcta");
+    }
+    else{
+        alert("La cedula ingresada es incorrecta");
+    }
+}
+btnVerificar.addEventListener("click",verificarCedula);
